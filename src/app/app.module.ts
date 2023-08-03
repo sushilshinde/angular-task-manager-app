c3885cb396bf324db28da55a93b6b4d60faba9b8
Murthy Kolati <kvarahamurty@msystechnologies.com>
2023-08-03 16:58:51 +0530

    implemented-json-model-service-api-call

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { BacklogComponent } from './home/backlog/backlog.component';
@@ -23,9 +24,11 @@ import {
   imports: [
     BrowserModule,
     AppRoutingModule,
+    HttpClientModule,
     CdkDrag,
     CdkDropList,
-    CdkDropListGroup
+    CdkDropListGroup,
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
