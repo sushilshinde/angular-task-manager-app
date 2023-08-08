588901e4347bbf2f8663c81f1b105691250d3c2e
Rajesh-Dukka <rajeshnaidu886@gmail.com>
2023-08-08 18:02:51 +0530

    changed styling

--- src/app/home/menubar/menubar.component.ts
+++ src/app/home/menubar/menubar.component.ts
@@ -6,6 +6,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./menubar.component.css'],
 })
 export class MenubarComponent {
+  menuItems = [
+    { icon: 'group', label: 'Manage', route: '' },
+    { icon: 'dns', label: 'Boards', route: '' },
+    { icon: 'event_note', label: 'Schedule', route: '' },
+    { icon: 'assessment', label: 'Reports', route: '' },
+  ];
+
   badgevisible = false;
   badgevisibility() {
     this.badgevisible = true;
