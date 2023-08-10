5394249fdf8b66eda84725609f6067c37ba66150
saikat-2022 <sacharjya@msystechnologies.com>
2023-08-10 12:36:27 +0530

    modification of folder structure and few changes in UI

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,12 +10,12 @@ import { HomeComponent } from './modules/admin/components/home/home.component';
 import { BacklogComponent } from './modules/admin/components/home/backlog/backlog.component';
 
 import {
-  CdkDrag,
-  CdkDropList,
-  CdkDragDrop,
-  moveItemInArray,
-  CdkDropListGroup,
-  transferArrayItem,
+    CdkDrag,
+    CdkDropList,
+    CdkDragDrop,
+    moveItemInArray,
+    CdkDropListGroup,
+    transferArrayItem,
 } from '@angular/cdk/drag-drop';
 import { NewListComponent } from './new-list/new-list.component';
 import { NewTaskComponent } from './new-task/new-task.component';
@@ -27,44 +27,44 @@ import { LoginComponent } from './components/login/login.component';
 import { NotFoundComponent } from './components/not-found/not-found.component';
 import { SignupComponent } from './components/signup/signup.component';
 import { MaterrialDesignModule } from './materrial-design/materrial-design.module';
-import { HighlightDirective } from './highlight.directive';
-// import { HomeComponent } from './modules/admin/components/home/home.component';
+import { HighlightDirective } from './custom-directive/highlight.directive';
+
 
 
 
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HomeComponent,
-    BacklogComponent,
-    NewListComponent,
-    NewTaskComponent,
-    MenubarComponent,
-    SidenavComponent,
-    SignupComponent,
-    LoginComponent,
-    ForgotPasswordComponent,
-    NotFoundComponent,
-    HomeComponent,
-    HighlightDirective,
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    HttpClientModule,
-    CdkDrag,
-    CdkDropList,
-    CdkDropListGroup,
-    BrowserAnimationsModule,
-    MaterialModule,
-    MaterrialDesignModule,
-    ReactiveFormsModule,
-    HttpClientModule,
-    BrowserAnimationsModule,
-    MaterialModule,
-  ],
-  providers: [],
-  bootstrap: [AppComponent],
+    declarations: [
+        AppComponent,
+        HomeComponent,
+        BacklogComponent,
+        NewListComponent,
+        NewTaskComponent,
+        MenubarComponent,
+        SidenavComponent,
+        SignupComponent,
+        LoginComponent,
+        ForgotPasswordComponent,
+        NotFoundComponent,
+        HomeComponent,
+        HighlightDirective,
+    ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        HttpClientModule,
+        CdkDrag,
+        CdkDropList,
+        CdkDropListGroup,
+        BrowserAnimationsModule,
+        MaterialModule,
+        MaterrialDesignModule,
+        ReactiveFormsModule,
+        HttpClientModule,
+        BrowserAnimationsModule,
+        MaterialModule,
+    ],
+    providers: [],
+    bootstrap: [AppComponent],
 })
-export class AppModule {}
+export class AppModule { }
