b712f4cf560a7dbf5a0bcab75951ff423ed52005
Murthy Kolati <kvarahamurty@msystechnologies.com>
2023-08-11 18:17:24 +0530

    added local storage functinality

--- src/app/modules/admin/components/home/backlog/backlog.component.ts
+++ src/app/modules/admin/components/home/backlog/backlog.component.ts
@@ -1,4 +1,8 @@
-import { CdkDragDrop, moveItemInArray, transferArrayItem } from '@angular/cdk/drag-drop';
+import {
+  CdkDragDrop,
+  moveItemInArray,
+  transferArrayItem,
+} from '@angular/cdk/drag-drop';
 import { Component, OnInit } from '@angular/core';
 import { TasksDb } from 'src/app/models/tasks.model';
 import { TaskService } from 'src/app/services/task.service';
@@ -6,34 +10,45 @@ import { TaskService } from 'src/app/services/task.service';
 @Component({
   selector: 'app-backlog',
   templateUrl: './backlog.component.html',
-  styleUrls: ['./backlog.component.css']
+  styleUrls: ['./backlog.component.css'],
 })
-export class BacklogComponent implements OnInit{
+export class BacklogComponent implements OnInit {
   all_tasks?: TasksDb[];
 
-  constructor(private allList: TaskService){}
+  constructor(private allList: TaskService) {}
 
   ngOnInit(): void {
-    this.allList.get_all_tasks().subscribe((res:any) => {
-      this.all_tasks = res.all_tasks;
-      console.log(this.all_tasks);
-    })
+    // getting the all task data from local storage
+    const local_data = localStorage.getItem('all_tasks');
+
+    // checking if all task are available or not in local storage
+    if (local_data != null) {
+      this.all_tasks = JSON.parse(local_data);
+    } else {
+      this.allList.get_all_tasks().subscribe((res: any) => {
+        this.all_tasks = res.all_tasks;
+        console.log(this.all_tasks); 
+      });
+    }
   }
 
   drop(event: CdkDragDrop<any>) {
     if (event.previousContainer === event.container) {
-      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+      moveItemInArray(
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex
+      );
     } else {
       transferArrayItem(
         event.previousContainer.data,
         event.container.data,
         event.previousIndex,
-        event.currentIndex,
+        event.currentIndex
       );
     }
     console.log('drag and drop event occurs');
+    localStorage.setItem('all_tasks', JSON.stringify(this.all_tasks));
     console.log(this.all_tasks);
-
   }
-
 }
