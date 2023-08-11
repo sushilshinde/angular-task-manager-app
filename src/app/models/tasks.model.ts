4bcbb5436ab0784c9d3c7f25edb1b0a98b5d14e8
saikat-2022 <sacharjya@msystechnologies.com>
2023-08-11 15:07:07 +0530

    change

--- src/app/models/tasks.model.ts
+++ src/app/models/tasks.model.ts
@@ -1,39 +1,38 @@
 export interface TasksDb {
-    backlog_tasks: BacklogTask[]
-    inprogress_tasks: InprogressTask[]
-    review_tasks: ReviewTask[]
-    completed_tasks: CompletedTask[]
-  }
-  
-  export interface BacklogTask {
-    priority: string
-    title: string
-    class: string
-    comments: string[]
-    avatar: string
-  }
-  
-  export interface InprogressTask {
-    priority: string
-    title: string
-    class: string
-    comments: string[]
-    avatar: string
-  }
-  
-  export interface ReviewTask {
-    priority: string
-    title: string
-    class: string
-    comments: string[]
-    avatar: string
-  }
-  
-  export interface CompletedTask {
-    priority: string
-    title: string
-    class: string
-    comments: string[]
-    avatar: string
-  }
-  
+  backlog_tasks: BacklogTask[]
+  inprogress_tasks: InprogressTask[]
+  review_tasks: ReviewTask[]
+  completed_tasks: CompletedTask[]
+}
+
+export interface BacklogTask {
+  priority: string
+  title: string
+  class: string
+  comments: string[]
+  avatar: string
+}
+
+export interface InprogressTask {
+  priority: string
+  title: string
+  class: string
+  comments: string[]
+  avatar: string
+}
+
+export interface ReviewTask {
+  priority: string
+  title: string
+  class: string
+  comments: string[]
+  avatar: string
+}
+
+export interface CompletedTask {
+  priority: string
+  title: string
+  class: string
+  comments: string[]
+  avatar: string
+}
