fc621b1470e775e30efbffef131aeaaf439cb938
Murthy Kolati <kvarahamurty@msystechnologies.com>
2023-08-11 15:02:41 +0530

    structure changed json file

--- src/app/models/tasks.model.ts
+++ src/app/models/tasks.model.ts
@@ -1,39 +1,12 @@
 export interface TasksDb {
-    backlog_tasks: BacklogTask[]
-    inprogress_tasks: InprogressTask[]
-    review_tasks: ReviewTask[]
-    completed_tasks: CompletedTask[]
+  category: string,
+  tasks: Task[]
   }
   
-  export interface BacklogTask {
+  export interface Task {
     priority: string
     title: string
     class: string
     comments: string[]
     avatar: string
   }
-  
-  export interface InprogressTask {
-    priority: string
-    title: string
-    class: string
-    comments: string[]
-    avatar: string
-  }
-  
-  export interface ReviewTask {
-    priority: string
-    title: string
-    class: string
-    comments: string[]
-    avatar: string
-  }
-  
-  export interface CompletedTask {
-    priority: string
-    title: string
-    class: string
-    comments: string[]
-    avatar: string
-  }
-  
