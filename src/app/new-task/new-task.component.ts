ca78d67d445ead19c914a0e80fc850d41a64577c
Murthy Kolati <kvarahamurty@msystechnologies.com>
2023-08-24 16:13:32 +0530

    merge

--- src/app/new-task/new-task.component.ts
+++ src/app/new-task/new-task.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms'
 import { Router } from '@angular/router';
 import { Task, TasksDb } from '../models/tasks.model';
 
