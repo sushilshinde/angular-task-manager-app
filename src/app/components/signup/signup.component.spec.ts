08bf971d757ccb7845e71e77b200271a07498950
saikat-2022 <sacharjya@msystechnologies.com>
2023-08-25 12:02:32 +0530

    add MatSnackBa for unit testr

--- src/app/components/signup/signup.component.spec.ts
+++ src/app/components/signup/signup.component.spec.ts
@@ -1,6 +1,7 @@
 import { HttpClientModule } from '@angular/common/http';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { ReactiveFormsModule } from '@angular/forms';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { StoreModule } from '@ngrx/store';
 import { of } from 'rxjs';
@@ -23,6 +24,8 @@ describe('SignupComponent', () => {
         MaterrialDesignModule,
         ReactiveFormsModule,
         BrowserAnimationsModule,
+        MatSnackBarModule,
+        
       ]
     });
     fixture = TestBed.createComponent(SignupComponent);
