205cf9bf2f8d42402feed7201bf2f0e5eda705ea
saikat-2022 <sacharjya@msystechnologies.com>
2023-08-30 10:35:47 +0530

    unit testing

--- src/app/services/task.service.spec.ts
+++ src/app/services/task.service.spec.ts
@@ -1,16 +1,19 @@
-// import { TestBed } from '@angular/core/testing';
+import { HttpClientModule } from '@angular/common/http';
+import { TestBed } from '@angular/core/testing';
 
-// import { TaskService } from './task.service';
+import { TaskService } from './task.service';
 
-// describe('TaskService', () => {
-//   let service: TaskService;
+describe('TaskService', () => {
+  let service: TaskService;
 
-//   beforeEach(() => {
-//     TestBed.configureTestingModule({});
-//     service = TestBed.inject(TaskService);
-//   });
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+        imports:[HttpClientModule],
+    });
+    service = TestBed.inject(TaskService);
+  });
 
-//   it('should be created', () => {
-//     expect(service).toBeTruthy();
-//   });
-// });
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+});
