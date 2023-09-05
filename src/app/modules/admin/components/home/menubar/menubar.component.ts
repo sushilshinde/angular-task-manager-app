168681a89e4d9c47a335432305c54a613ebe70c6
Rajesh-Dukka <rajeshnaidu886@gmail.com>
2023-09-05 12:23:47 +0530

    fixing errors

--- src/app/modules/admin/components/home/menubar/menubar.component.ts
+++ src/app/modules/admin/components/home/menubar/menubar.component.ts
@@ -1,4 +1,4 @@
-import { AfterViewInit, Component, OnDestroy, ViewChild } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
 import { AuthService } from 'src/app/services/auth.service';
 import { Router } from '@angular/router';
 import { MatDialog } from '@angular/material/dialog';
@@ -7,27 +7,14 @@ import { Store } from '@ngrx/store';
 
 import * as AuthActions from 'src/app/store/auth.actions';
 import { selectLoggedInUser } from 'src/app/store/auth.selectors';
-import {
-  concatMap,
-  debounceTime,
-  distinctUntilChanged,
-  EMPTY,
-  filter,
-  map,
-  pluck,
-  Subscription,
-  switchMap,
-} from 'rxjs';
-import { NgForm } from '@angular/forms';
-import { SearchService } from 'src/app/services/search.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-menubar',
   templateUrl: './menubar.component.html',
   styleUrls: ['./menubar.component.css'],
 })
-export class MenubarComponent implements AfterViewInit, OnDestroy {
-  searchKey: String = '';
+export class MenubarComponent implements OnDestroy {
   loginTime: Date | null; // Initialize as null
   loggedInUser$ = this.store.select(selectLoggedInUser);
 
@@ -38,15 +25,10 @@ export class MenubarComponent implements AfterViewInit, OnDestroy {
     { icon: 'assessment', label: 'Reports', route: '' },
   ];
 
-  loading: boolean = false;
   badgevisible = false;
   badgevisibility() {
     this.badgevisible = true;
   }
-
-  @ViewChild('searchForm') searchForm?: NgForm;
-  searchResults?: any;
-  searchResultCount: any;
 
   private subscription!: Subscription;
 
@@ -54,8 +36,7 @@ export class MenubarComponent implements AfterViewInit, OnDestroy {
     private auth: AuthService,
     private router: Router,
     private dialogRef: MatDialog,
-    private store: Store,
-    private _searchService: SearchService
+    private store: Store
   ) {
     this.loginTime = auth.getLoginTime(); // Initialize loginTime
     this.subscription = this.loggedInUser$.subscribe((user) => {
@@ -70,50 +51,6 @@ export class MenubarComponent implements AfterViewInit, OnDestroy {
 
   addNewTask() {
     this.dialogRef.open(NewTaskComponent);
-  }
-
-  ngAfterViewInit(): void {
-    // this._searchService.getSearches('Onboarding design').subscribe(res =>{
-    //   console.log()
-    // })
-
-    const formValue = this.searchForm?.valueChanges;
-
-    formValue
-      ?.pipe(
-        // map(data => data.searchTerm)
-        //or
-        // map(data => data['searchTerm'])
-        //or pluck
-        filter(() => !!this.searchForm?.valid),
-        // pluck('searchTerm'),
-        map((data) => data.searchTerm),
-        debounceTime(500),
-        distinctUntilChanged(),
-        switchMap((data) => {
-          this.loading = true; // Show spinner while loading
-          return this._searchService.getSearches(data);
-        })
-      )
-      .subscribe((res) => {
-        console.log('Search results :', res);
-        this.searchResults = res;
-        this.searchResultCount = Object.keys(res).length;
-        this.loading = false; // Hide spinner once data is loaded
-      });
-  }
-
-  getColorByPriority(priority: string): string {
-    switch (priority) {
-      case 'low-priority':
-        return '#5196f6';
-      case 'high-priority':
-        return '#ce5388';
-      case 'medium-priority':
-        return '#6bc8d1';
-      default:
-        return 'black';
-    }
   }
 
   ngOnDestroy() {
